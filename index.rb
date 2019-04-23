8a3d56d100d841d5841a0fca9fcc531c98349c62
Ewe Lin Loo <ewelinloo@gmail.com>
2019-04-23 14:58:38 +1000

    Add correct formatting for grid

--- index.rb
+++ index.rb
@@ -1,15 +1,20 @@
 require "tty-box"
 
-box = TTY::Box.frame(width: 30, height: 14, title: {top_left: "Don\'t Break", bottom_right: 'the Chain'}, align: :center, padding: 1) do
-"xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx
-xxxxxxxxxx"
+grid = []
+count = 0
+
+def fixed_array(size, content)
+    Array.new(size){|i| content[i]}
 end
-print box
+
+while count < 10
+    grid.push(fixed_array(10, [count]).join(" "))
+    count += 1
+end
+
+grid[0][5] = "x"
+
+box = TTY::Box.frame(width: 20, height: 14, title: {top_left: "Don\'t Break", bottom_right: 'the Chain'}, align: :center, padding: 1) {
+"#{grid.join("\n")}"
+}
+puts box
