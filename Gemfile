d3039e222504b35d2e1db94182b6f815b95a94ee
Ewe Lin Loo <ewelinloo@gmail.com>
2019-04-23 21:16:56 +1000

    Add add and delete functions

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "tty-box", "~> 0.3.0"
+gem 'tty-prompt'
--- index.rb
+++ index.rb
@@ -1,20 +1,53 @@
 require "tty-box"
+require "tty-prompt"
 
-grid = []
+def menu(num2, num1 = 0)
+    system "clear"
+    puts "What would you like to do?"
+        @menu = [
+            {"View" => -> { view }},
+            {"Add" => -> { add(num1, num2) }},
+            {"Delete" => -> { delete(num1, num2) }},
+            {"Update" => -> { update }}]
+    prompt = TTY::Prompt.new
+    function = prompt.select("", @menu)
+end
+
 count = 0
+@grid = []
 
 def fixed_array(size, content)
     Array.new(size){|i| content[i]}
 end
 
 while count < 10
-    grid.push(fixed_array(10, [count]).join(" "))
+    @grid.push(fixed_array(10, [count]).join(" "))
     count += 1
 end
 
-grid[0][5] = "x"
-
-box = TTY::Box.frame(width: 20, height: 14, title: {top_left: "Don\'t Break", bottom_right: 'the Chain'}, align: :center, padding: 1) {
-"#{grid.join("\n")}"
+@box = TTY::Box.frame(width: 20, height: 14, title: {top_left: "Don\'t Break", bottom_right: 'the Chain'}, align: :center, padding: 1) {
+"#{@grid.join("\n")}"
 }
-puts box
+
+def view
+    puts @box
+end
+
+def add(y, x = 0)
+    @grid[x][y] = "x"
+    @box = TTY::Box.frame(width: 20, height: 14, title: {top_left: "Don\'t Break", bottom_right: 'the Chain'}, align: :center, padding: 1) {
+"#{@grid.join("\n")}"
+}
+    puts @box
+end
+
+def delete(y, x=0)
+    @grid[x][y] = " "
+    @box = TTY::Box.frame(width: 20, height: 14, title: {top_left: "Don\'t Break", bottom_right: 'the Chain'}, align: :center, padding: 1) {
+"#{@grid.join("\n")}"
+}
+    puts @box
+end
+
+day = 0.to_s.split(//)
+menu(day[0].to_i, day[1].to_i)
