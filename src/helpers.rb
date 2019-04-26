b9d80bd477fcfd0fef04de04bd3fa4d434dd9634
Ewe Lin Loo <ewelinloo@gmail.com>
2019-04-26 14:57:35 +1000

    Create ui.rb

--- src/helpers.rb
+++ src/helpers.rb
@@ -13,4 +13,25 @@ module Helpers
     def write_file
         File.open('marks.txt', 'w') { |f| f.puts(@grid)}
     end
+
+    def track_day
+        # TD: Knowing that .open returns the block and .write returns length
+        # TD: Getting the initial date to reset appropriately
+        # TD: Getting the correct time to use in subtraction
+        unless File.exists?('init.txt')
+            @init_txt = File.open('init.txt', 'w+') { |f| f.write Date.today }
+        end
+    
+        init_time = File.birthtime('init.txt')
+        init_date = init_time.to_i / (60 * 60 * 24)
+        # Add to current date to test day tracking function
+        current_date = (Time.now.utc.to_i / (60 * 60 * 24)) + 99
+        
+        day_num = current_date - init_date
+        if day_num.nil? || day_num >= 100 || day_num < 0
+            day_num = 0
+        end
+        @day = format('%02d', day_num)
+        @day_index = @day.to_s.split(//)
+    end
 end
