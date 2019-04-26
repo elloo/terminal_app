9ae24f1de690502bd9852411ef4006e2cffdb728
Ewe Lin Loo <ewelinloo@gmail.com>
2019-04-26 20:48:53 +1000

    Create final version of readme and add docs

--- src/helpers.rb
+++ src/helpers.rb
@@ -1,8 +1,11 @@
 module Helpers
+    
+    # Used to create an array of ten blank elements.
     def fixed_array(size, content)
         Array.new(size){|i| content[i]}
     end
 
+    # Creates a 2D array from the lines of marks contained in marks.txt
     def read_file
         @grid = []
         File.open('marks.txt').readlines.each do |line|
@@ -10,28 +13,41 @@ module Helpers
         end
     end
 
+    # Writes the marks stored in @grid to the marks.txt file
     def write_file
         File.open('marks.txt', 'w') { |f| f.puts(@grid)}
     end
 
+    # Used to keep count of days 0 - 99. 
+    # Keeping it coder themed by counting from 0
     def track_day
-        # TD: Knowing that .open returns the block and .write returns length
-        # TD: Getting the initial date to reset appropriately
-        # TD: Getting the correct time to use in subtraction
+
+        # Create the init.txt file unless it already exists
         unless File.exists?('init.txt')
             @init_txt = File.open('init.txt', 'w+') { |f| f.write Date.today }
         end
     
+        # Keep track of when the file was created
         init_time = File.birthtime('init.txt')
+        # Convert the time of file creation into days
         init_date = init_time.to_i / (60 * 60 * 24)
-        # Add to current date to test day tracking function
+        # Convert the current time into days
+        # Note that Time.now had to be used instead of Date.today so that it could be subtracted by the File.birthtime
+        # (Add to current date to test day tracking function)
         current_date = Time.now.utc.to_i / (60 * 60 * 24)
         
+        # Calculate the current day by subtracting current_date from init_date
         day_num = current_date - init_date
+
+        # In case of edge cases
         if day_num.nil? || day_num >= 100 || day_num < 0
             day_num = 0
         end
+
+        # Making sure that single-digit numbers can be accepted
         @day = format('%02d', day_num)
+
+        # Splitting numbers into two separate elements in an array
         @day_index = @day.to_s.split(//)
     end
 end
--- src/ui.rb
+++ src/ui.rb
@@ -7,25 +7,30 @@ class Menu
     end
 
     def menu(num1, num2)
+        # Reset to create marks.txt and init.txt files
         unless File.exists?('marks.txt')
             reset
         end
-        # system "clear"
+        system "clear"
+        # Read file to get latest version of marks in 2D array (@grid)
         read_file
+
+        # TTY-Box syntax to create border with marks printed inside
         @box = TTY::Box.frame(width: 20, height: 12, title: {top_left: "Don\'t Break", bottom_right: 'the Chain'}, align: :center) {
     "#{@grid.join("\n")}"
     }
         puts @box
 
+        # @day used from track_day method in helpers.rb
         puts "Day #{@day}"
             @menu = [
                 {"Mark" => -> { 
-                    puts "num1 is  #{num1}" 
-                    puts  "num2 is  #{num2}"
+                    # puts "num1 is  #{num1}" 
+                    # puts  "num2 is  #{num2}"
                 mark(num1, num2){@grid[num1][num2] = "x"} }},
                 {"Delete" => -> { mark(num1, num2){@grid[num2][num1] = " "} }},
                 {"Reset" => -> { reset }},
-                {"Quit" => -> { # TD: Sometimes quitting twice
+                {"Quit" => -> { # Problem: Sometimes needs quitting twice
                     system "clear" 
                     break }}]
         prompt = TTY::Prompt.new
@@ -34,27 +39,35 @@ class Menu
 
     def mark(x, y)
         read_file
-        puts " x is #{x}" 
-        print "y is #{y}"
+        # puts " x is #{x}" 
+        # print "y is #{y}"
+        # Using yield saves the creation of an extra delete method
         yield(x, y)
         write_file
         menu(@day_index[0].to_i, @day_index[1].to_i)
     end
     
     def reset
+        # Delete init.txt as track_day relies on creation date
         if File.exists?('init.txt')
             File.delete('init.txt')
             @init_txt = nil
             track_day
         end
+
+        # Start count to keep track of arrays pushed to @grid array
         count = 0
         # reinitialize the grid to empty upon reset and update array from 0-9
+        # Using @grid to form a 2D array. This makes it easy to assign a day to a specific "x" mark. e.g. Day 00 could easily be turned into day_index[0]. day_index[0]
         @grid = []
         while count < 10
+            # fixed_array from helpers.rb to create arrays with ten blank elements
             @grid.push(fixed_array(10, [""]).join(" "))
             count += 1
         end
+        # Saving the empty 2D array to marks.txt
         write_file
+        # Using @day_index array from track_day method in helpers.rb
         menu(@day_index[0].to_i, @day_index[1].to_i)
     end
 end
